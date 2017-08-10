b8c6f19e5cc61f278c1d12d662d36065e1b52336
pt-br <lucas.paixao@objectedge.com>
2017-08-10 01:05:50 -0300

    Home ajustments

--- app/scripts/pages/home/home.js
+++ app/scripts/pages/home/home.js
@@ -3,20 +3,29 @@ const { massiveHide } = require('../../coreFunctions');
 const home = {
   init: () => {
     constructor();
+    setupTopicList();
   },
 };
 
 const constructor = () => {
   body.addClass('mo-home');
   homeContainer = body.find('#content');
+  const actionContainer = body.find('#actionTopicsContent');
+  const paginationContainer = actionContainer.find('#paginacao');
 
   const hideElements = [
     homeContainer.find('#goto'),
+    actionContainer.find('.left'),
   ];
   
   massiveHide(hideElements);
 
   homeContainer.addClass('mo-home-container');
+};
+
+const setupTopicList = () => {
+  const topicsContainer = homeContainer.find('#topics');
+  topicsContainer.addClass('mo-topics-container');
 };
 
 module.exports = home;

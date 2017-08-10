beed618763852233438bd09c778d979dddc35736
pt-br <lucas.paixao@objectedge.com>
2017-08-10 00:46:09 -0300

    Initial footer

--- app/scripts/sections/footer.js
+++ app/scripts/sections/footer.js
@@ -1,18 +1,36 @@
+const { massiveHide } = require('../coreFunctions'); 
+
 const footer = {
   init: () => {
     constructor();
   },
 };
 
-constructor = () => {
-  const footerContainer = body.find('#footer');
-  const footerContent = footerContainer.find('#footer-menu');
+const constructor = () => {
+  // footerContainer = body.find('#footer');
+  const tempFooter = body.find('#footer');
+  const newFooterContent = `
+    <div class="mo-footer-container">
+      <a href="/new_topic.jbb?forum.id=57" class="fa fa-plus-square fa-2x mo-footer-button"></a>
+      <a onclick="alert('Meus Tópicos - Em Breve')" class="fa fa-comments fa-2x mo-footer-button"></a>
+      <a onclick="alert('MPs - Em Breve')" class="fa fa-envelope fa-2x mo-footer-button"></a>
+      <a onclick="alert('Tópicos Favoritos - Em Breve')" class="fa fa-heart fa-2x mo-footer-button"></a>
+    </div>
+  `;
+  tempFooter.after(newFooterContent);
+  footerContainer = body.find('.mo-footer-container');
 
-  footerContainer.addClass('mo-footer');
-  footerContent.addClass('mo-footer-content');
+  const hideElements = [
+    body.find('#importTab1'),
+    body.find('#estatisticas'),
+    body.find('#legendas'),
+    body.find('#footer'),
+    tempFooter,
+  ];
+  
+  massiveHide(hideElements);
 
-  const desktopHost = footerContent.find('div');
-  desktopHost.remove();
+  footerContainer.addClass('mo-footer-container');
 }
 
 module.exports = footer;
